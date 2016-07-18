e5d7984fbd6c650def8e03fcbb41e25c6a602b32
myemcu <1142497680@qq.com>
2016-07-18 16:45:23 +0800

    V2，网格视图显示SD卡中图片。

--- app/src/main/java/com/myemcu/pictureapp/MainActivity.java
+++ app/src/main/java/com/myemcu/pictureapp/MainActivity.java
@@ -2,17 +2,26 @@ package com.myemcu.pictureapp;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 import static android.Manifest.permission.*; // 手打，new String[]{READ_EXTERNAL_STORAGE}用
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final int REQUEST_READ_STORAGE = 3; // 定义私有静态整形常量
+    SimpleCursorAdapter adapter;                       // 定义游标适配器,专在涉及数据库查询中用
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void readThumbnails() { // 读取缩略图
+
+        // 获取网格对象
+        GridView grid = (GridView) findViewById(R.id.grid); //控件id可与控件对象重名
+        // 定义显示来源
+        String[] from = {MediaStore.Images.Thumbnails.DATA, MediaStore.Images.Media.DISPLAY_NAME};
+        // 定义显示目地
+        int[] to = new int[]{R.id.thumb_image, R.id.thumb_text};
+        // 创建适配器对象
+        adapter = new SimpleCursorAdapter(getBaseContext(),R.layout.thumb_item,null,from,to,0);
+        grid.setAdapter(adapter);
+        // 使用CursorLoader节约存取开销(存取外部SD卡)
+        getLoaderManager().initLoader(0, null, this); // this红浪后按Alt+Enter键生成下面的3个方法
     }
 
     @Override
@@ -52,5 +73,25 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // 保存查询的资料位置
+        Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        // 产生并返回资料读取器(CursorLoader)对象,并将uri传递给它
+        return new CursorLoader(this, uri, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // 当资料读取器(CursorLoader)在内容提供者中查询完成时,会自动呼叫onLoadFinished()方法,即呼叫
+        // adapter的swapCursor()方法替换adapter内的Cursor对象。
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
