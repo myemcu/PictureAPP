24ba69087f223b417ac55f0d0ec3fc6513a460fa
myemcu <1142497680@qq.com>
2016-07-19 00:20:23 +0800

    V4，Clean Project后的滑屏切图。

--- app/src/main/java/com/myemcu/pictureapp/DetailActivity.java
+++ app/src/main/java/com/myemcu/pictureapp/DetailActivity.java
@@ -7,18 +7,24 @@ import android.provider.MediaStore;
 import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements GestureDetector.OnGestureListener{
 
     private int position;
     private ImageView image;
     private Cursor cursor; // 数据库查询结果
 
+    GestureDetector detector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        detector = new GestureDetector(this, this);
 
         position = getIntent().getIntExtra("POSITION", 0);
         image = (ImageView) findViewById(R.id.imageView);
@@ -30,9 +36,60 @@ public class DetailActivity extends AppCompatActivity {
         updateImage();
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return detector.onTouchEvent(event);
+    }
+
     private void updateImage() {
         String imagePath = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
         Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
         image.setImageBitmap(bitmap);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    // 快速滑屏后释放
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+        float distance = e2.getX()-e1.getX();
+
+        if (distance > 100) { //右滑屏(向前翻)
+            if (!cursor.moveToPrevious()) {
+                cursor.moveToLast();
+            }
+            updateImage();
+        }else if (distance < -100) { //左滑屏(向后翻)
+            if (!cursor.moveToNext()) {
+                cursor.moveToFirst();
+            }
+            updateImage();
+        }
+
+        return false;
     }
 }
